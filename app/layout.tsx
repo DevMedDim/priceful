6a4fb98481b2f9afb5821f1517a0ecbd3f982849
AkMedIm <mrakdimdev@gmail.com>
2024-08-08 00:37:03 +0100

    setting up the preject, and navbar

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Priceful",
+    description: "Keep eyes on your price",
+    icons: {
+        icon: "/favicon.ico",
+        shortcut: "/favicon-16x16.png",
+        apple: "/apple-touch-icon.png",
+    },
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <main className="max-w-10xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <Navbar />
+                    {children}
+                </main>
+            </body>
+        </html>
+    );
+}
